a2f114345c849bebda9f8a673efa9028e82c1276
Jens Hahne <jehahne@uni-wuppertal.de>
2019-08-04 16:30:00 +0200

    Rename machine meshes

--- python_mgrit/sample_runs/run_machine.py
+++ python_mgrit/sample_runs/run_machine.py
@@ -15,20 +15,20 @@ if __name__ == '__main__':
 
         np.save('results/' + name + '/' + str(self.t[0][0]) + '-' + str(self.t[0][-1]), sol)
 
-    machine_0 = im_3kW.InductionMachine(nonlinear=False, pwm=False, grid='im_3kW_16k',
+    machine_0 = im_3kW.InductionMachine(nonlinear=False, pwm=False, grid='im_3kW_17k',
                                         t_start=0, t_stop=0.0002, nt=65)
     first_level = np.hstack(
         (np.arange(0, len(machine_0.t))[::4], np.arange(0, len(machine_0.t))[::4][1:] - 1))
     first_level.sort()
     machine_0.t = machine_0.t[first_level]
     machine_0.nt = len(first_level)
-    machine_1 = im_3kW.InductionMachine(nonlinear=False, pwm=False, grid='im_3kW_16k',
+    machine_1 = im_3kW.InductionMachine(nonlinear=False, pwm=False, grid='im_3kW_17k',
                                         t_start=0, t_stop=0.0002, nt=17)
     machine_2 = im_3kW.InductionMachine(nonlinear=False, pwm=False, grid='im_3kW_4k',
                                         t_start=0, t_stop=0.0002, nt=5)
 
     problem = [machine_0, machine_1, machine_2]
     transfer = [grid_transfer_copy.GridTransferCopy(),
-                grid_transfer_machine.GridTransferMachine(fine_grid='im_3kW_16k', coarse_grid='im_3kW_4k')]
+                grid_transfer_machine.GridTransferMachine(fine_grid='im_3kW_17k', coarse_grid='im_3kW_4k')]
     mgrit = solver.MgritFas(problem=problem, transfer=transfer)
     result2 = mgrit.solve()
