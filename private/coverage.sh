fc23174cc36c55f9e24b47a5ab7cf2966252c833
Tim Helfensdörfer <t.helfensdoerfer@uni-wuppertal.de>
2019-08-07 14:53:34 +0200

    Added gitlab CI file

--- private/coverage.sh
+++
@@ -1,2 +0,0 @@
-python3 -m coverage run --source=mgrit/ setup.py
-python3 -m coverage html
--- private/scp.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-rsync -avh --exclude 'private' --exclude '__pycache__' --exclude 'results' --exclude 'PyMgrit.egg-info' /home/jens/uni/pasirom/python_mgrit hahne@wmwrsrv2.math.uni-wuppertal.de:/home/hahne
