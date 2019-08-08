0a2db59ce23ed51483a80b82be20b8512cb86ea3
Jens Hahne <jehahne@uni-wuppertal.de>
2019-08-08 10:44:10 +0200

    Delete plots etc

--- private/scp.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-rsync -avh --exclude 'private' --exclude '__pycache__' --exclude '.git' --exclude 'results' --exclude 'PyMgrit.egg-info' /home/jens/uni/pymgrit hahne@wmwrsrv2.math.uni-wuppertal.de:/home/hahne
