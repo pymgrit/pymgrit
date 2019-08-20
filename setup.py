76a8ae45c3092c6d807e119ca3c6580d4b51131f
Jens Hahne <jehahne@uni-wuppertal.de>
2019-08-20 11:14:19 +0200

    Update setup file

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
-from setuptools import setup
+import os
+import sys
+import subprocess
+
+import setuptools
+from setuptools import setup, find_packages, Extension
+from setuptools.command.build_ext import build_ext
+from setuptools.command.test import test as TestCommand
+
+install_requires = (
+    'numpy>=1.17.0',
+    'scipy>=1.3.0',
+    'mpi4py>=3.0'
+)
 
 
 def readme():
@@ -14,10 +27,8 @@ setup(name='PyMGRIT',
       author='Jens Hahne',
       author_email='jens.hahne@math.uni-wuppertal.de',
       license='MIT',
-      packages=['pymgrit'],
-      install_requires=[
-          'numpy', 'scipy', 'mpi4py'
-      ],
+      packages=find_packages(exclude=['doc']),
+      install_requires=install_requires,
       include_package_data=True,
       test_suite='nose.collector',
       tests_require=['nose', 'nose-cover3'],
