63c2c78eba2f08ac2784ccab302c5a7bc2ce8135
Tim Helfensdörfer <tim@visualappeal.de>
2020-01-15 13:14:06 +0100

    Added classes to the init script

--- src/pymgrit/__init__.py
+++ src/pymgrit/__init__.py
@@ -1,5 +1,33 @@
+from .advection_equation.advection_equation import AdvectionEquation
+from .cable_current_driven.cable_current_driven import CableCurrentDriven
+from .cable_voltage_driven.cable_voltage_driven import CableVoltageDriven
+from .core.application import Application
+from .core.grid_transfer import GridTransfer
+from .core.grid_transfer_copy import GridTransferCopy
+from .core.mgrit import Mgrit
 from .core.vector import Vector
+from .core.vector_standard import VectorStandard
+from .heat_equation.heat_equation import HeatEquation
+from .heat_equation_bdf2.heat_equation_2pts_bdf1 import HeatEquation as HeatEquationBDF1
+from .heat_equation_bdf2.heat_equation_2pts_bdf2 import HeatEquation as HeatEquationBDF2
+from .heat_equation_bdf2.vector_standard_bdf2 import VectorStandardBDF2
+from .parallel_model.mgrit_parallel_model import MgritParallelModel
+from .parallel_model.parallel_model import ParallelModel
 
 __all__ = [
-    Vector
+    AdvectionEquation,
+    CableCurrentDriven,
+    CableVoltageDriven,
+    Application,
+    GridTransfer,
+    GridTransferCopy,
+    Mgrit,
+    Vector,
+    VectorStandard,
+    HeatEquation,
+    HeatEquationBDF1,
+    HeatEquationBDF2,
+    VectorStandardBDF2,
+    MgritParallelModel,
+    ParallelModel
 ]
