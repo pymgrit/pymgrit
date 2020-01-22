e051b8e1f607ff866ae3713832580eb2a2957542
Tim Helfensdörfer <tim@visualappeal.de>
2020-01-22 11:21:50 +0100

    Added sample docs

--- build_docs.sh
+++ build_docs.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-apt update
-apt install -y mpich
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    apt update
+    apt install -y mpich
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install mpich
+fi
 
 pip install .
-pip install sphinx
+pip install sphinx sphinx-rtd-theme
 sphinx-apidoc -o docs/source src/pymgrit
 sphinx-build -b html docs/source docs/build/html
 
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,8 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
+
 sys.path.insert(0, os.path.abspath('../../src/pymgrit'))
 
 
@@ -31,7 +33,8 @@ release = '0.1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc'
+    'sphinx.ext.autodoc',
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -48,7 +51,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
