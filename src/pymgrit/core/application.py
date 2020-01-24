3a0ae051e5540a8aae6e1b0656249b00ff5d8df8
Jens Hahne <jehahne@uni-wuppertal.de>
2020-01-24 11:26:13 +0100

    Changed structure of application base class, such that it is possible to insert intervall instead of t_stop,t_start and nt Added tests for application constructor Removed continue-on-error from tests action

--- src/pymgrit/core/application.py
+++ src/pymgrit/core/application.py
@@ -10,18 +10,31 @@ class Application(ABC):
     Abstract class for problems. Each problem has to be a child
     """
 
-    def __init__(self, t_start: float, t_stop: float, nt: int) -> None:
+    def __init__(self, t_start: float = None, t_stop: float = None, nt: int = None,
+                 t_interval: np.ndarray = None) -> None:
         """
         Initiate the time information
         :param t_start: Start point
         :param t_stop: End point
         :param nt: Number of time points
         """
-        self.t_start = t_start
-        self.t_end = t_stop
-        self.nt = nt
-        self.t = np.linspace(self.t_start, self.t_end, nt)
-        self._u = None
+
+        if t_interval is None:
+            if t_start is None or t_stop is None or nt is None:
+                raise Exception('Specify an interval by t_start, t_stop and nt or by t_interval')
+            self.t_start = t_start
+            self.t_end = t_stop
+            self.nt = nt
+            self.t = np.linspace(self.t_start, self.t_end, nt)
+            self._u = None
+        else:
+            if not isinstance(t_interval, np.ndarray):
+                raise Exception('t_interval has the wrong type. Should be a numpy array')
+            self.t_start = t_interval[0]
+            self.t_end = t_interval[-1]
+            self.nt = len(t_interval)
+            self.t = t_interval
+            self._u = None
 
     @property
     def u(self):
