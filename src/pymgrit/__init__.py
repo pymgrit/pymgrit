abc4b58abd71414e72e3107e287fec8a558dfcfb
Tim Helfensdörfer <tim@visualappeal.de>
2020-03-03 13:23:36 +0100

    Fixed class import

--- src/pymgrit/__init__.py
+++ src/pymgrit/__init__.py
@@ -1,16 +1,19 @@
 from .advection.advection_1d import Advection1D
+from .arenstorf_orbit.arenstorf_orbit import ArenstorfOrbit
 from .core.application import Application
 from .core.grid_transfer import GridTransfer
 from .core.grid_transfer_copy import GridTransferCopy
 from .core.mgrit import Mgrit
+from .core.simple_setup_problem import simple_setup_problem
 from .core.vector import Vector
+from .dahlquist.dahlquist import Dahlquist
 from .heat.heat_1d import Heat1D
 from .heat.heat_2d import Heat2D
-from pymgrit.heat.heat_1d_2pts_bdf1 import Heat1DBDF1
-from pymgrit.heat.heat_1d_2pts_bdf2 import Heat1DBDF2
-from pymgrit.heat.vector_heat_1d_2pts import VectorHeat1D2Pts
+from .heat.heat_1d_2pts_bdf1 import Heat1DBDF1
+from .heat.heat_1d_2pts_bdf2 import Heat1DBDF2
+from .heat.vector_heat_1d_2pts import VectorHeat1D2Pts
+from .lorentz.lorentz import Lorentz
 from .parallel_model.mgrit_parallel_model import MgritParallelModel
 from .parallel_model.parallel_model import ParallelModel
-from .core.simple_setup_problem import simple_setup_problem
 
 __all__ = [s for s in dir() if not s.startswith('_')]
