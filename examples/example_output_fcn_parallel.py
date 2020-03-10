10a17fe958583de89f7e550474dd5df04534940e
Jens <jens.hahne@math.uni-wuppertal.de>
2020-03-10 21:51:27 +0100

    added parallel output function example

--- examples/example_output_fcn_parallel.py
+++ examples/example_output_fcn_parallel.py
@@ -5,6 +5,9 @@ Access and plot the solution
 import pathlib
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+
+from mpi4py import MPI
 
 from pymgrit.brusselator.brusselator import Brusselator
 from pymgrit.core.mgrit import Mgrit
@@ -13,11 +16,11 @@ from pymgrit.core.mgrit import Mgrit
 def main():
     def output_fcn(self):
         # Set path to solution
-        path = 'results/' + 'brusselator' + '/' + str(self.solve_iter)
+        path = 'results/' + 'brusselator_parallel' + '/' + str(self.solve_iter)
         # Create path if not existing
         pathlib.Path(path).mkdir(parents=True, exist_ok=True)
         # Save solution to file; here, we have two solution values at each time point.
-        np.save(path + '/brusselator',
+        np.save(path + '/brusselator-' + str(self.comm_time_rank),
                 [self.u[0][i].get_values() for i in self.index_local[0]])  # Solution values at local time points
 
     # Create two-level time-grid hierarchy for the Brusselator system
@@ -35,15 +38,20 @@ def main():
     cols = 2
     rows = iterations_needed // cols + iterations_needed % cols
     position = range(1, iterations_needed + 1)
+    rank = MPI.COMM_WORLD.Get_rank()
 
-    fig = plt.figure(1, figsize=[10,10])
-    # Plot the solution (Note: modifications necessary if more than one process is used for the simulation!)
-    for i in range(iterations_needed):
-        res = np.load('results/brusselator/' + str(i) + '/brusselator.npy', allow_pickle=True)
-        ax = fig.add_subplot(rows, cols, position[i])
-        ax.scatter(res[:,0], res[:,1])
-    plt.show()
-
+    if rank == 0:
+        fig = plt.figure(1, figsize=[10, 10])
+        for i in range(iterations_needed):
+            files = []
+            path = 'results/brusselator_parallel/'+str(i)+'/'
+            for filename in os.listdir(path):
+                files.append([int(filename[filename.find('-') + 1: -4]), np.load(path + filename, allow_pickle=True)])
+            files.sort(key=lambda tup: tup[0])
+            res = np.vstack([l.pop(1) for l in files])
+            ax = fig.add_subplot(rows, cols, position[i])
+            ax.scatter(res[:, 0], res[:, 1])
+        plt.show()
 
 if __name__ == '__main__':
     main()
