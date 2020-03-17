930dfb3c68bb397da1a008926827e7367f8b5fc9
Stephanie Friedhoff <friedhoff@math.uni-wuppertal.de>
2020-03-17 15:23:50 +0100

    edit of advanced mg hierarchy doc

--- examples/example_time_integrators.py
+++ examples/example_time_integrators.py
@@ -1,5 +1,5 @@
 """
-Different time integrators per MGRIT level
+Time-grid hierarchy with different time integrators
 """
 
 from pymgrit.dahlquist.dahlquist import Dahlquist
@@ -7,12 +7,12 @@ from pymgrit.core.mgrit import Mgrit
 
 
 def main():
-    # Create Dahlquist's test problem choosing implicit mid-point rule as time stepper
+    # Create Dahlquist's test problem using implicit mid-point rule time integration
     dahlquist_lvl0 = Dahlquist(t_start=0, t_stop=5, nt=101, method='MR')
-    # Create Dahlquist's test problem choosing implicit backward euler as time stepper
+    # Create Dahlquist's test problem using backward Euler time integration
     dahlquist_lvl1 = Dahlquist(t_start=0, t_stop=5, nt=51, method='BE')
 
-    # Setup MGRIT and solve the problem
+    # Setup an MGRIT solver and solve the problem
     mgrit = Mgrit(problem=[dahlquist_lvl0, dahlquist_lvl1])
     info = mgrit.solve()
 
