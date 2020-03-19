4c91c6ab9ed860d296b05f5a468897045f68bb55
Stephanie Friedhoff <friedhoff@math.uni-wuppertal.de>
2020-03-19 15:51:25 +0100

    updates in 2d heat equation example

--- examples/example_heat_2d.py
+++ examples/example_heat_2d.py
@@ -1,3 +1,16 @@
+"""
+Apply two-level MGRIT V-cycles with FCF-relaxation to solve the 2D heat equation
+    u_t - a(u_xx + u_yy) = b(x,y,t),  a > 0,
+        in [x_start, x_end] x [y_start, y_end] x (t_start, t_end],
+with RHS b(x,t) = -sin(pi*x)(sin(t) - a*pi^2*cos(t)),
+homogeneous Dirichlet BCs in space,
+    u(x_start, y) = u(x_end, y) = u(x, y_start) = u(x, y_end) = 0,
+and subject to the initial condition
+    u(x,0)  = sin(pi*x),    x in [0,1]
+
+=> exact solution u(x,t) = sin(pi*x)*cos(t)
+"""
+
 import pathlib
 import numpy as np
 import os
@@ -18,21 +31,41 @@ def main():
         np.save(path + '/heat_equation_2d-rank' + str(self.comm_time_rank),
                 [[[self.t[0][i], self.u[0][i]] for i in self.index_local[0]]])
 
-    lx = 0.75
-    ly = 1.5
+    # example problem parameters
+    x_end = 0.75
+    y_end = 1.5
     a = 3.5
 
-    def f(x, y, t):
-        return 5 * x * (lx - x) * y * (ly - y) + 10 * a * t * (y * (ly - y) + x * (lx - x))
+    def rhs(x, y, t):
+        """
+        Right-hand side of 2D heat equation example problem at a given space-time point (x,y,t),
+          5x(x_end - x)y(y_end - y) + 10at(y(y_end-y)) + x(x_end - x),  a = 3.5
 
-    heat0 = Heat2D(lx=lx, ly=ly, nx=55, ny=125, a=a, rhs=f, t_start=0, t_stop=1, nt=33)
-    heat1 = Heat2D(lx=lx, ly=ly, nx=55, ny=125, a=a, rhs=f, t_interval=heat0.t[::2])
+        :param x: x-coordinate of spatial grid point
+        :param y: y_coordinate of spatial grid point
+        :param t: time point
+        :return: right-hand side of 2D heat equation example problem at point (x,y,t)
+        """
 
+        return 5 * x * (x_end - x) * y * (y_end - y) + 10 * a * t * (y * (y_end - y) + x * (x_end - x))
+
+    def exact_sol(x, y, t):
+        """
+        Exact solution of 2D heat equation example problem at a given space-time point (x,y,t)
+        Note: Can be used for computing the error of the MGRIT approximation
+
+        :param x: x_coordinate of spatial grid point
+        :param y: y_coordinate of spatial grid point
+        :param t: time point
+        :return: exact solution of 2D heat equation example problem at point (x,y,t)
+        """
+        return 5 * t * x * (x_end - x) * y * (y_end - y)
+
+    heat0 = Heat2D(x_start=0, x_end=x_end, y_start=0, y_end=y_end, nx=55, ny=125, a=a, rhs=rhs, t_start=0, t_stop=1, nt=33)
+    heat1 = Heat2D(x_start=0, x_end=x_end, y_start=0, y_end=y_end, nx=55, ny=125, a=a, rhs=rhs, t_interval=heat0.t[::2])
+
+    # Setup two-level MGRIT solver and solve the problem
     mgrit = Mgrit(problem=[heat0, heat1], cycle_type='V', output_fcn=output_fcn)
-
-    def u_exact(x, y, t):
-        return 5 * t * x * (lx - x) * y * (ly - y)
-
     info = mgrit.solve()
 
     if MPI.COMM_WORLD.Get_rank() == 0:
@@ -45,7 +78,7 @@ def main():
 
         diff = 0
         for i in range(len(sol)):
-            u_e = u_exact(x=heat0.x_2d, y=heat0.y_2d, t=heat0.t[i])
+            u_e = exact_sol(x=heat0.x_2d, y=heat0.y_2d, t=heat0.t[i])
             diff += abs(sol[i][1].get_values() - u_e).max()
         print("Difference between MGRIT solution and exact solution:", diff)
 
--- src/pymgrit/heat/heat_2d.py
+++ src/pymgrit/heat/heat_2d.py
@@ -1,5 +1,10 @@
 """
-Heat equation 2-d example
+Vector and application class for the 2D heat equation
+
+Time integration methods:
+   Forward Euler
+   Backward Euler
+   Crank-Nicolson
 """
 
 import numpy as np
@@ -14,73 +19,142 @@ from pymgrit.core.vector import Vector
 
 class VectorHeat2D(Vector):
     """
-    Vector for the 2D heat equation
+    Vector class for the 2D heat equation
     """
 
     def __init__(self, nx, ny):
+        """
+        Constructor.
+
+        :param nx: number of degrees of freedom in x-direction
+        :param ny: number of degrees of freedom in y-direction
+        """
         super(VectorHeat2D, self).__init__()
         self.nx = nx
         self.ny = ny
         self.values = np.zeros((self.nx, self.ny))
 
     def __add__(self, other):
+        """
+        Addition of two vector objects (self and other)
+
+        :param other: vector object to be added to self
+        :return: sum of vector object self and input object other
+        """
         tmp = VectorHeat2D(self.nx, self.ny)
         tmp.set_values(self.get_values() + other.get_values())
         return tmp
 
     def __sub__(self, other):
+        """
+        Subtraction of two vector objects (self and other)
+
+        :param other: vector object to be subtracted from self
+        :return: difference of vector object self and input object other
+        """
         tmp = VectorHeat2D(self.nx, self.ny)
         tmp.set_values(self.get_values() - other.get_values())
         return tmp
 
     def norm(self):
+        """
+        Norm of a vector object
+
+        :return: 2-norm of vector object
+        """
         return np.linalg.norm(self.values)
 
     def clone_zero(self):
+        """
+        Initialize vector object with zeros
+
+        :rtype: vector object with zero values
+        """
         return VectorHeat2D(self.nx, self.ny)
 
     def clone_rand(self):
+        """
+        Initialize vector object with random values
+
+        :rtype: vector object with random values
+        """
         tmp = VectorHeat2D(self.nx, self.ny)
         tmp.set_values(np.random.rand((self.nx, self.ny)))
         return tmp
 
     def set_values(self, values):
+        """
+        Set vector data
+
+        :param values: values for vector object
+        """
         self.values = values
 
     def get_values(self):
+        """
+        Get vector data
+
+        :return: values of vector object
+        """
         return self.values
 
 
 class Heat2D(Application):
     """
-    Application class for the heat equation in 2D space
-    Time discretization schemes:
-      Forward Euler
-      Backward Euler
-      Crank-Nicolson
+    Application class for the heat equation in 2D space,
+        u_t - a(u_xx + u_yy) = b(x,y,t),  a > 0,
+        in [x_start, x_end] x [y_start, y_end] x (t_start, t_end],
+    with homogeneous Dirichlet boundary conditions in space.
     """
 
-    def __init__(self, lx: float, ly: float, nx: int, ny: int, a: float, rhs: Callable = lambda x, y, t: 0 * x * y,
-                 init_con_fcn: Callable = lambda x, y: x * y * 0, method: str = 'BE',
-                 u_b_0x: Union[int, float, Callable] = 0, u_b_0y: Union[int, float, Callable] = 0,
-                 u_b_lx: Union[int, float, Callable] = 0, u_b_ly: Union[int, float, Callable] = 0, *args, **kwargs):
-        super(Heat2D, self).__init__(*args, **kwargs)
+    def __init__(self, x_start: float, x_end: float, y_start: float, y_end: float, nx: int, ny: int,
+                 a: float, rhs: Callable = lambda x, y, t: 0 * x * y,
+                 init_cond: Callable = lambda x, y: x * y * 0, method: str = 'BE',
+                 bc_left: Union[int, float, Callable] = 0, bc_right: Union[int, float, Callable] = 0,
+                 bc_bottom: Union[int, float, Callable] = 0, bc_top: Union[int, float, Callable] = 0, *args, **kwargs):
+        """
+        Constructor.
 
-        self.nx = nx
-        self.ny = ny
-        self.x = np.linspace(0, lx, self.nx)
-        self.y = np.linspace(0, ly, self.ny)
-        self.dx = self.x[1] - self.x[0]
-        self.dy = self.y[1] - self.y[0]
-        self.a = a
-        self.lx = lx
-        self.ly = ly
+        :param x_start: left bound of x-domain
+        :param x_end: right bound of x-domain
+        :param y_start: left bound of y-domain
+        :param y_end: right bound of y-domain
+        :param nx: number of points in x-direction
+        :param ny: number of points in y-direction
+        :param a: thermal conductivity
+        :param rhs: right-hand side
+        :param init_cond: initial condition
+        :param method: method for solving Dahlquist's equation:
+                       'BE' -> Backward Euler (default)
+                       'FE' -> Forwared Euler
+                       'CN' -> Crank-Nicolson
+        :param bc_left: boundary condition for left boundary
+        :param bc_right: boundary condition for right boundary
+        :param bc_bottom: boundary condition for bottom boundary
+        :param bc_top: boundary condition for top boundary
+        """
+        super(Heat2D, self).__init__(*args, **kwargs)
+        # Spatial domain (including boundary points)
+        self.x_start = x_start
+        self.x_end = x_end
+        self.y_start = y_start
+        self.y_end = y_end
+        self.x = np.linspace(x_start, x_end, nx)
+        self.y = np.linspace(x_end, y_end, ny)
         self.x_2d = self.x[:, np.newaxis]
         self.y_2d = self.y[np.newaxis, :]
-        self.fx = (self.a / self.dx ** 2)
-        self.fy = (self.a / self.dy ** 2)
+        self.nx = nx
+        self.ny = ny
+        self.dx = self.x[1] - self.x[0]
+        self.dy = self.y[1] - self.y[0]
+
+        # Thermal conductivity
+        self.a = a
+
+        # Set right-hand side routine
         self.rhs = rhs
 
+        # Set time integration method
         if method == 'BE':
             self.theta = 1
         elif method == 'FE':
@@ -90,95 +164,78 @@ class Heat2D(Application):
         else:
             raise Exception("Unknown method. Choose BE (Backward Euler), FE (Forward Euler) or CN (Crank-Nicolson")
 
-        # Set boundary conditions
-        if isinstance(u_b_0x, (float, int)):
-            self.u_b_0x: Callable = lambda t: u_b_0x
-        elif callable(u_b_0x):
-            self.u_b_0x: Callable = u_b_0x
+        # Set Dirichlet boundary conditions
+        if isinstance(bc_left, (float, int)):
+            self.bc_left: Callable = lambda y: bc_left
+        elif callable(bc_left):
+            self.bc_left: Callable = bc_left
         else:
-            raise Exception("Choose float, int or function for boundary condition u_b_0x")
+            raise Exception("Choose float, int or function for boundary condition bc_left")
 
-        if isinstance(u_b_0y, (float, int)):
-            self.u_b_0y: Callable = lambda t: u_b_0y
-        elif callable(u_b_0y):
-            self.u_b_0y: Callable = u_b_0y
+        if isinstance(bc_right, (float, int)):
+            self.bc_right: Callable = lambda y: bc_right
+        elif callable(bc_right):
+            self.bc_right: Callable = bc_right
         else:
-            raise Exception("Choose float, int or function for boundary condition u_b_0x")
+            raise Exception("Choose float, int or function for boundary condition bc_right")
 
-        if isinstance(u_b_lx, (float, int)):
-            self.u_b_lx: Callable = lambda t: u_b_lx
-        elif callable(u_b_lx):
-            self.u_b_lx: Callable = u_b_lx
+        if isinstance(bc_bottom, (float, int)):
+            self.bc_bottom: Callable = lambda x: bc_bottom
+        elif callable(bc_bottom):
+            self.bc_bottom: Callable = bc_bottom
         else:
-            raise Exception("Choose float, int or function for boundary condition u_b_0x")
+            raise Exception("Choose float, int or function for boundary condition bc_bottom")
 
-        if isinstance(u_b_ly, (float, int)):
-            self.u_b_ly: Callable = lambda t: u_b_ly
-        elif callable(u_b_ly):
-            self.u_b_ly: Callable = u_b_ly
+        if isinstance(bc_top, (float, int)):
+            self.bc_top: Callable = lambda x: bc_top
+        elif callable(bc_top):
+            self.bc_top: Callable = bc_top
         else:
-            raise Exception("Choose float, int or function for boundary condition u_b_0x")
+            raise Exception("Choose float, int or function for boundary condition bc_top")
 
+        # Set (spatial) identity matrix and spatial discretization matrix
         self.space_disc = self.compute_matrix()
         self.identity = identity(self.nx * self.ny, dtype='float', format='csr')
 
+        # Set the data structure for any user-defined time point
         self.vector_template = VectorHeat2D(self.nx, self.ny)
-        self.vector_t_start = VectorHeat2D(self.nx, self.ny)  # Create initial value solution
-        self.vector_t_start.set_values(init_con_fcn(self.x_2d, self.y_2d))  # Set initial value
 
-    def compute_rhs(self, u_start: np.ndarray, t_start: float, t_stop: float):
-        """
-        Define right hand side corresponding to time discretization
-        """
-        b = np.zeros((self.nx, self.ny))
-
-        b[:, 0] = self.u_b_0x(t_stop)
-        b[:, -1] = self.u_b_lx(t_stop)
-        b[0, :] = self.u_b_0y(t_stop)
-        b[-1, :] = self.u_b_ly(t_stop)
-
-        if self.theta == 1:
-            b[1:-1, 1:-1] = u_start[1:-1, 1:-1] + \
-                            self.theta * (t_stop - t_start) * self.rhs(x=self.x_2d[1:-1], y=self.y_2d[:, 1:-1],
-                                                                       t=t_stop)
-        else:
-            b[1:-1, 1:-1] = u_start[1:-1, 1:-1] + \
-                            (1 - self.theta) * \
-                            ((self.fx * (t_stop - t_start) * (
-                                    u_start[2:, 1:-1] - 2 * u_start[1:-1, 1:-1] + u_start[:-2, 1:-1])) +
-                             (self.fy * (t_stop - t_start) * (
-                                     u_start[1:-1, 2:] - 2 * u_start[1:-1, 1:-1] + u_start[1:-1, :-2]))) + \
-                            self.theta * (t_stop - t_start) * self.rhs(x=self.x_2d[1:-1], y=self.y_2d[:, 1:-1],
-                                                                       t=t_stop) + \
-                            (1 - self.theta) * (t_stop - t_start) * self.rhs(x=self.x_2d[1:-1], y=self.y_2d[:, 1:-1],
-                                                                             t=t_start)
-
-        return b.T.flatten()
+        # Set initial condition
+        self.init_cond = init_cond
+        self.vector_t_start = VectorHeat2D(self.nx, self.ny)
+        self.vector_t_start.set_values(self.init_cond(self.x_2d, self.y_2d))
 
     def compute_matrix(self):
         """
         Define spatial discretization matrix for 2D heat equation
-        Second-order central finite differences in space.
+
+        Second-order central finite differences with matrix stencil
+            [          -f_y          ]
+            [-f_x  2(f_x + f_y)  -f_x]
+            [          -f_y          ]
+        with f_x = (a / dx^2) and f_y = (a / dy^2)
         """
+        fx = self.a / self.dx ** 2
+        fy = (self.a / self.dy ** 2)
         n = self.nx * self.ny
 
-        main = np.ones(n) * (2 * self.theta * (self.fx + self.fy))
+        main = np.ones(n) * (2 * (fx + fy))
         main[:self.nx] = main[-self.nx:] = 0
         main[self.nx::self.nx] = main[2 * self.nx - 1::self.nx] = 0
 
-        upper = np.ones(n - 1) * -self.theta * self.fx
+        upper = np.ones(n - 1) * (-fx)
         upper[:self.nx] = upper[-self.nx:] = 0
         upper[self.nx::self.nx] = upper[2 * self.nx - 1:: self.nx] = 0
 
-        lower = np.ones(n - 1) * -self.theta * self.fx
+        lower = np.ones(n - 1) * (-fx)
         lower[:self.nx - 1] = lower[-self.nx:] = 0
         lower[self.nx - 1::self.nx] = lower[2 * self.nx - 2:: self.nx] = 0
 
-        upper2 = np.ones(n - self.nx) * -self.theta * self.fy
+        upper2 = np.ones(n - self.nx) * (-fy)
         upper2[:self.nx] = 0
         upper2[self.nx::self.nx] = upper2[2 * self.nx - 1:: self.nx] = 0
 
-        lower2 = np.ones(n - self.nx) * -self.theta * self.fy
+        lower2 = np.ones(n - self.nx) * (-fy)
         lower2[-self.nx:] = 0
         lower2[::self.nx] = lower2[self.nx - 1::self.nx] = 0
 
@@ -187,20 +244,84 @@ class Heat2D(Application):
                        shape=(n, n), format='csr')
         return matrix
 
+    def compute_rhs(self, u_start: np.ndarray, t_start: float, t_stop: float):
+        """
+        Right-hand side of spatial system for implicit time integratio nmethods
+
+        :param u_start: approximate solution for the input time t_start
+        :param t_start: time associated with the input approximate solution u_start
+        :param t_stop: time to evolve the input approximate solution to
+        :return: right-hand side of spatial system at each time step in case of implicit time integration
+        """
+
+        # Set boundary conditions
+        b = np.zeros((self.nx, self.ny))
+
+        b[0, :] = self.bc_left(self.y_2d)
+        b[-1, :] = self.bc_right(self.y_2d)
+        b[:, 0] = self.bc_bottom(self.x_2d)
+        b[:, -1] = self.bc_top(self.x_2d)
+
+        if self.theta == 1:
+            # BE
+            b[1:-1, 1:-1] = u_start[1:-1, 1:-1] + \
+                            (t_stop - t_start) * self.rhs(x=self.x_2d[1:-1], y=self.y_2d[:, 1:-1], t=t_stop)
+        else:
+            # CN: theta = 1/2
+            tmp = (self.identity - self.theta * (t_stop - t_start) * self.space_disc) * u_start.flatten()
+            b += tmp.reshape(self.ny, self.nx).T
+
+            # add RHS of PDE
+            b[1:-1, 1:-1] += self.theta * (t_stop - t_start) * self.rhs(x=self.x_2d[1:-1], y=self.y_2d[:, 1:-1],
+                                                                        t=t_stop) + \
+                             (1 - self.theta) * (t_stop - t_start) * self.rhs(x=self.x_2d[1:-1], y=self.y_2d[:, 1:-1],
+                                                                              t=t_start)
+
+        return b.T.flatten()
+
     def step(self, u_start: VectorHeat2D, t_start: float, t_stop: float) -> VectorHeat2D:
         """
         Time integration routine for 2D heat equation example problem:
-            Backward Euler in time
-            Forward Euler in time
-            Crank-Nicolson in time
+            Backward Euler (BE)
+            Forward Euler (FE)
+            Crank-Nicolson (CN)
+
+        One-step method
+           (u_i - u_{i-1})/dt + (theta*L*u_i + (1-theta)*L*u_{i-1} = theta*b_i + (1-theta)*b_{i-1},
+        where L = self.space_disc is the spatial discretization operator and
+           theta =  0  -> FE
+           theta = 1/2 -> CN
+           theta =  1  -> BE
 
         :param u_start: approximate solution for the input time t_start
         :param t_start: time associated with the input approximate solution u_start
         :param t_stop: time to evolve the input approximate solution to
         :return: approximate solution at input time t_stop
         """
-        rhs = self.compute_rhs(u_start=u_start.get_values(), t_start=t_start, t_stop=t_stop)
-        new = spsolve((t_stop - t_start) * self.space_disc + self.identity, rhs)
+        if self.theta == 0:
+            # FE
+            old = u_start.get_values()
+            new = np.zeros((self.nx, self.ny))
+
+            # Set BCs
+            new[0, :] = self.bc_left(self.y_2d)
+            new[-1, :] = self.bc_right(self.y_2d)
+            new[:, 0] = self.bc_bottom(self.x_2d)
+            new[:, -1] = self.bc_top(self.x_2d)
+
+            tmp = (self.identity - (t_stop - t_start) * self.space_disc) * old.flatten()
+            new += tmp.reshape(self.nx, self.ny).T
+
+            # add RHS of PDE
+            new[1:-1, 1:-1] += (t_stop - t_start) * self.rhs(x=self.x_2d[1:-1], y=self.y_2d[:, 1:-1], t=t_start)
+
+            new.T.flatten()
+
+        else:
+            # set up and solve linear system
+            rhs = self.compute_rhs(u_start=u_start.get_values(), t_start=t_start, t_stop=t_stop)
+            new = spsolve((t_stop - t_start) * self.theta * self.space_disc + self.identity, rhs)
+
         ret = VectorHeat2D(self.nx, self.ny)
         ret.set_values(new.reshape(self.ny, self.nx).T)
         return ret
