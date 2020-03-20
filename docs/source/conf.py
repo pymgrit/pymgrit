f9fcf8a63e05054df6198edf4e9381a84a681504
Tim Helfensdörfer <tim@visualappeal.de>
2020-03-20 11:31:39 +0100

    Version bump

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2020, Jens Hahne'
 author = 'Jens Hahne'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.2'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
