9f2b8fef16d7a36331cc568339bc0654fae12212
Tim Helfensdörfer <tim@visualappeal.de>
2020-03-25 15:41:02 +0100

    Removed oversubscribe option and output mpi version

--- tests/mpi/mpi.sh
+++ tests/mpi/mpi.sh
@@ -2,4 +2,6 @@
 
 DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-mpiexec --oversubscribe --use-hwthread-cpus -np $1 ${DIR}/../../venv/bin/python3 ${DIR}/../../examples/example_dahlquist.py
+mpiexec --version
+
+mpiexec --use-hwthread-cpus -np $1 ${DIR}/../../venv/bin/python3 ${DIR}/../../examples/example_dahlquist.py
