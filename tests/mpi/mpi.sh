c7d3f99daa2cce8d6a5f34a5ffa72bf6efd05f4b
Tim Helfensdörfer <tim@visualappeal.de>
2020-03-25 15:56:05 +0100

    mpich does not know the hyperthreads parameter

--- tests/mpi/mpi.sh
+++ tests/mpi/mpi.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 mpiexec --version
 
-mpiexec --use-hwthread-cpus -np $1 ${DIR}/../../venv/bin/python3 ${DIR}/../../examples/example_dahlquist.py
+mpiexec -n $1 ${DIR}/../../venv/bin/python3 ${DIR}/../../examples/example_dahlquist.py
